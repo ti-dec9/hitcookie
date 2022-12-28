9aea24f16c75feac906a17ae2640691439c22b78
Katsu <xjverudonx@gmail.com>
2022-12-28 10:46:05 -0300

    Correção na seção novidades

--- app/Views/components/Novidades.php
+++ app/Views/components/Novidades.php
@@ -7,7 +7,7 @@
 
 
 <div class="grid grid-cols-1 lg:grid-cols-3">
-    <div class="w-full card rounded-md">
+    <div class="w-full card">
         <figure><img class="w-10/12" src="https://ik.imagekit.io/jdmxypilt/HitCookie/WhatsApp_Image_2022-12-01_at_22.51.51__1__NwSBQ6Crz.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1671473457389" alt="Shoes" /></figure>
         <div class="card-body bg-amarelohk">
           <div class="block">
@@ -16,7 +16,7 @@
         <p class="leading-6 opacity-80 text-verdehk">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur itaque eligendi accusamus corrupti? Voluptate ullam <br> <br> <span class="font-bold leading-12 hover-underline-animation2"><a href="">Ler Mais</a></span></p>
         </div>
       </div>
-      <div class="w-full card rounded-md">
+      <div class="w-full card">
         <figure><img class="w-10/12" src="https://ik.imagekit.io/jdmxypilt/HitCookie/WhatsApp_Image_2022-12-01_at_22.51.51_yuM9nUbsy.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1671473909388" alt="Shoes" /></figure>
         <div class="card-body bg-amarelohk">
           <div class="block">
@@ -25,7 +25,7 @@
         <p class="leading-6 opacity-80 text-verdehk">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur itaque eligendi accusamus corrupti? Voluptate ullam <br> <br> <span class="font-bold leading-12 hover-underline-animation2"><a href="">Ler Mais</a></span></p>
         </div>
       </div>
-      <div class="w-full card rounded-md">
+      <div class="w-full card">
         <figure><img class="w-10/12" src="https://ik.imagekit.io/jdmxypilt/HitCookie/WhatsApp_Image_2022-12-01_at_22.51.54__3__0AI9i6pGZ.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1671474079004" alt="Shoes" /></figure>
         <div class="card-body bg-amarelohk">
           <div class="block">
