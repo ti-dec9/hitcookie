ad567be384590f1ca8ec49831bbc36e9daa2bdec
Lucas Oliveira Vieira Cardoso <lucascardoso5263@gmail.com>
2023-01-05 11:03:46 -0300

    alterações feitas

--- app/Views/components/Cardapiohome.php
+++ app/Views/components/Cardapiohome.php
@@ -2,8 +2,8 @@
 <section class="relative flex items-center justify-center py-20 pt-48 lg:pt-20 lg:pl-32 bg-verdehk">
     <!-- Linha preta abaixo do tÃ­tulo -->
    <div class="relative z-20 px-4 flex flex-row lg:py-0 py-20 w-[750px] h-full mx-4 border-4 rounded-lg border-amarelohk lg:mx-0 lg:pr-4">
-    <div class="absolute inset-x-0 -top-72 lg:-inset-y-20 lg:-left-72">
-      <img class="lg:w-[550px] lg:h-[550px] w-[550px] h-[550px]  lg:flex drop-shadow-xl " src="https://ik.imagekit.io/jdmxypilt/HitCookie/IMG_4420-removebg__1__KHtHs_dri.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672416363073" alt="" />
+    <div class="absolute inset-x-0 -top-72 lg:-inset-y-[106px] lg:-left-60">
+      <img class="lg:w-[450px] lg:h-[600px] w-[550px] h-[550px]  lg:flex drop-shadow-xl " src="https://ik.imagekit.io/jdmxypilt/HitCookie/IMG_4420-removebg__1__KHtHs_dri.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672416363073" alt="" />
     </div>
         <div class="relative z-40 py-16 text-center lg:ml-52 lg:px-10 lg:text-start">
       <h2 class="pt-4 text-2xl font-bold text-white uppercase">Conheça nosso Cardápio!</h2> <br>
