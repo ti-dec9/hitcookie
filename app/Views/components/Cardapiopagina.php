aaef966550067566fa0558810fe2333df61faa9d
Katsu <xjverudonx@gmail.com>
2023-01-05 15:33:34 -0300

    Alteração cardapio

--- app/Views/components/Cardapiopagina.php
+++ app/Views/components/Cardapiopagina.php
@@ -108,6 +108,57 @@
   </div>
 </div>
 
+
+<!-- Categoria 3 --> <br>
+<div tabindex="0" class="collapse collapse-arrow border border-verdehk bg-base-100 rounded-box">
+  <div class="collapse-title text-xl font-medium text-verdehk">
+    Bebidas
+  </div>
+  <div class="collapse-content"> 
+    
+    <div class="grid grid-cols-2 gap-0">
+
+    <div class="lg:py-4 py-2">
+    <figure><img class="lg:w-10/12 lg:h-full w-[400px] h-[200px]" src="https://ik.imagekit.io/jdmxypilt/HitCookie/Banner_Hit_Cookie-03_PWkpfgkPt.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672421398406" alt="Misto Quente" /></figure>
+    </div>
+
+    <div class="lg:pt-2 px-4">
+      <h1 class="text-lg text-verdehk font-bold lg:py-2">Misto Quente</h1> 
+      <p class="lg:py-2">Preparado de uma maneira especial, o presunto e o queijo derretido são uma explosão de sabor na boca.</p> 
+      <span class="text-md text-verdehk font-bold lg:py-2"> R$20,00</span>
+    </div>
+
+
+    <div class="lg:py-4 py-2">
+    <figure><img class="lg:w-10/12 lg:h-full w-[400px] h-[200px]" src="https://ik.imagekit.io/jdmxypilt/HitCookie/Banner_Hit_Cookie-03_PWkpfgkPt.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672421398406" alt="Misto Quente" /></figure>
+    </div>
+
+    <div class="lg:pt-2 px-4">
+      <h1 class="text-lg text-verdehk font-bold lg:py-2">Misto Quente</h1> 
+      <p class="lg:py-2">Preparado de uma maneira especial, o presunto e o queijo derretido são uma explosão de sabor na boca.</p> 
+      <span class="text-md text-verdehk font-bold lg:py-2"> R$20,00</span>
+    </div>
+
+
+    <div class="lg:py-4 py-2">
+    <figure><img class="lg:w-10/12 lg:h-full w-[400px] h-[200px]" src="https://ik.imagekit.io/jdmxypilt/HitCookie/Banner_Hit_Cookie-03_PWkpfgkPt.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672421398406" alt="Misto Quente" /></figure>
+    </div>
+
+    <div class="lg:pt-2 px-4">
+      <h1 class="text-lg text-verdehk font-bold lg:py-2">Misto Quente</h1> 
+      <p class="lg:py-2">Preparado de uma maneira especial, o presunto e o queijo derretido são uma explosão de sabor na boca.</p> 
+      <span class="text-md text-verdehk font-bold lg:py-2"> R$20,00</span>
+    </div>
+
+</div>
+
+
+    
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
   
   
 
